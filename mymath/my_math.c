076772805e90f24e68bb6bbfd4ae8e1b621e38a8
Austin Adams <git@austinjadams.com>
2017-12-17 09:16:55 -0500

    Add mymath example config

---
+++ mymath/my_math.c
@@ -0,0 +1,11 @@
+// Example homework file to test (solution)
+// If you say cfiles=my_math.c in zucc.config, this will be overwritten by
+// the student's copy
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
